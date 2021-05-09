b595f83b632c0bddaa783389d9531b248a2d5277
JoannaGo <80228495+JoannaGo@users.noreply.github.com>
2021-05-09 18:15:45 +0900

    Added search engine

--- src/app.js
+++ src/app.js
@@ -34,8 +34,19 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+function search(city) {
 let apiKey = "30506a58371f72a45aab62f40ca6cec5";
-let city = "Paris";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
 axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+search("Rome");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
